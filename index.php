3723ba9534b34fc3374ce6b0f87edcef8c1137dc
realcryptonight <me@realcryptonight.dev>
2022-09-27 10:34:57 +0200

    Added: index and disclaimer footer

---
+++ index.php
@@ -0,0 +1,15 @@
+<html>
+	<head>
+		<title>NerdyGedgets</title>
+		<link rel="stylesheet" href="./css/index.css">
+	</head>
+	<body>
+		<div class="footer">
+			<div class="footer-text">
+				<p>This is a school project for <a href="https://www.windesheim.com/">Windesheim.</a></p>
+				<p>No copyright infringement intended.</p>
+				<p>In case of copyright infringement please contact <a href="mailto:me@realcryptonight.dev">me@realcryptonight.dev</a></p>
+			</div>
+		</div>
+	</body>
+</html>
